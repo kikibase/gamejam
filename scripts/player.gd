aebe9717ec566d16fb994fc12fc247665637ac5d
k333sh <123787240+k333sh@users.noreply.github.com>
2023-01-28 11:48:20 -0600

    updatandmovefix

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,29 +1,28 @@
 extends KinematicBody2D
 
-export var speed_multipler = 500
+const SPEED_MULTIPLIER = 500
 
-var velocity: Vector2 = Vector2()
-var direction: Vector2 = Vector2()
+var velocity = Vector2()
+var direction = Vector2()
 
-func _physics_process(_delta: float) -> void:
+func _physics_process(delta : float )-> void :
 	read_input()
 
+func read_input():
+	if Input.is_action_pressed("up"):
+		move_player(0, -1)
+	elif Input.is_action_pressed("down"):
+		move_player(0, 1)
+	elif Input.is_action_pressed("left"):
+		move_player(-1, 0)
+	elif Input.is_action_pressed("right"):
+		move_player(1, 0)
 
-func read_input() -> void:
-	if Input.is_action_just_pressed("up"):#check if input is the up 
-		move_player(0,1)
-	if Input.is_action_just_pressed("down"):
-		move_player(0,-1)
-	if Input.is_action_just_pressed("left"):
-		move_player(-1,0)
-	if Input.is_action_just_pressed("right"):
-		move_player(1,0	)
-		
+func move_player(x_coords, y_coords):
+	velocity.x += x_coords
+	velocity.y += y_coords
 
-func move_player(var x_coords, var y_coords) -> void:#dunction to move player
-	velocity.y+=y_coords#change character position in y-axis
-	velocity.x+=x_coords#change character position in x-axis
-	
-	direction = Vector2(x_coords,y_coords)#to set the current direction of the player
+	direction = Vector2(x_coords, y_coords)
 	velocity = velocity.normalized()
-	velocity = move_and_slide(velocity*speed_multipler)
+	move_and_slide(velocity * SPEED_MULTIPLIER)
+
