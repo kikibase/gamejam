d7027a946405311146fab0e3e6099b4f9b8695e0
kikibase <o.f.d169@gmail.com>
2023-01-28 14:25:22 -0600

    random changes

--- scripts/videoloop.gd
+++ scripts/videoloop.gd
@@ -1,10 +1,13 @@
 extends VideoPlayer        
 
-func _ready(): #{           
-	set_process(true)
-#}
 
-func _process(delta): #{
+func _ready():           
+	set_process(true)
+
+
+func _process(delta): 
 	if not is_playing():
 		play()
-#}
+
+func stop_video():
+	stop()
