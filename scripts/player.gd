8dbff615083c60100ad4639eda5893ec605f0c53
kikibase <o.f.d169@gmail.com>
2023-01-28 20:23:50 -0600

    changes

--- scripts/player.gd
+++ scripts/player.gd
@@ -11,11 +11,15 @@ func _physics_process(_delta: float) -> void:
 		i.targetBody = self 
 		i.isPlayer = true #depending on whether this is your controllable player
 	read_input()
+	#attack()
 	
 	
 
 
 func read_input() -> void:
+	if not Input.is_action_pressed("melee_attack") and not Input.is_action_pressed("up") and not Input.is_action_pressed("down") and not Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
+		if _animated_sprite.frame ==7:
+			_animated_sprite.play("idle")
 	if Input.is_action_pressed("up"):#check if input is the up 
 		move_player(0,-1)
 		_animated_sprite.play("walking")		
@@ -30,9 +34,13 @@ func read_input() -> void:
 		move_player(1,0)
 		_animated_sprite.play("walking")
 		_animated_sprite.flip_h = false
-	if not Input.is_action_pressed("up") and not Input.is_action_pressed("down") and not Input.is_action_pressed("left") and not Input.is_action_pressed("right"):
-		_animated_sprite.play("idle")
+	if Input.is_action_pressed("melee_attack") or (Input.is_action_pressed("melee_attack") and Input.is_action_pressed("right") ):
+		_animated_sprite.play("attack")
 		
+	
+		
+		
+	
 
 
 func move_player(x_coords, y_coords) -> void:#dunction to move player
@@ -47,3 +55,6 @@ func move_player(x_coords, y_coords) -> void:#dunction to move player
 	velocity = velocity.normalized()#normalizes the velocity to the current 
 # warning-ignore:return_value_discarded
 	move_and_slide(velocity*speed_multipler)
+	
+
+	
